8e60d90c9be7a865cff92a9840247bd7f034cd1e
mfdomain83 <87289136+mfdomain83@users.noreply.github.com>
2022-04-11 04:15:13 -0500

    File Fixes

--- src/mfdomain/print_my_name.py
+++ src/mfdomain/print_my_name.py
@@ -3,3 +3,5 @@
 def say_hi(message) -> str:
     if isinstance(message, str):
         return message
+    else:
+        return "INVALID"
--- tests/test_print_my_name.py
+++ tests/test_print_my_name.py
@@ -2,4 +2,5 @@ from src.mfdomain import print_my_name
 
 
 def test_say_hi():
-    assert print_my_name.say_hi(message="Hello") is "Hello"
+    assert print_my_name.say_hi(message="Hello") == "Hello"
+    assert print_my_name.say_hi(message=6) == "INVALID"
